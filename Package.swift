3b5bce290aa09d8b1f17b33a0df8c7fdb0147495
Mehrdad Faraji <mehrdadfaraji.cs@gmail.com>
2024-08-21 17:52:57 -0700

    Add new link

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "datasourceProgramContactInformation",
-         url: "https://kmm-shared-test.s3.us-east-2.amazonaws.com/datasourceProgramContactInformation.xcframework.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASFUIR2Q5Q5HE3RLT%2F20240821%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20240821T202132Z&X-Amz-Expires=14400&X-Amz-SignedHeaders=host&X-Amz-Signature=7c4956a3f94dacc744a45ecad3df1d534bc7c710fb384faa8265871167b5c3a9",
+         url: "https://kmm-shared-test.s3.us-east-2.amazonaws.com/datasourceProgramContactInformation.xcframework.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASFUIR2Q5Q5HE3RLT%2F20240822%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20240822T005027Z&X-Amz-Expires=14400&X-Amz-SignedHeaders=host&X-Amz-Signature=9f2f04ca38752dc1e637754c61a2f2341ef62b669599d11ba934aa68d4b2bc9e",
          checksum:"6bbfb1dbd03424a486d36714a32430992ef9b62da8019053ceb4eb7618f88b86")
    ]
 )
