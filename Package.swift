03ef3c30284736d3ec833ed0305eb1438c1cd4b5
Mehrdad Faraji <mehrdadfaraji.cs@gmail.com>
2024-08-26 10:01:40 -0700

    Update url for 2 days

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "datasourceProgramContactInformation",
-         url: "https://kmm-shared-test.s3.us-east-2.amazonaws.com/datasourceProgramContactInformation.xcframework.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASFUIR2Q5Q5HE3RLT%2F20240822%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20240822T005027Z&X-Amz-Expires=14400&X-Amz-SignedHeaders=host&X-Amz-Signature=9f2f04ca38752dc1e637754c61a2f2341ef62b669599d11ba934aa68d4b2bc9e",
+         url: "https://kmm-shared-test.s3.us-east-2.amazonaws.com/datasourceProgramContactInformation.xcframework.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASFUIR2Q5Q5HE3RLT%2F20240826%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20240826T165745Z&X-Amz-Expires=174000&X-Amz-SignedHeaders=host&X-Amz-Signature=760c4fb654448416ee70014846bb7d6350391f9118a3d4a658f3e52ab72cb597",
          checksum:"6bbfb1dbd03424a486d36714a32430992ef9b62da8019053ceb4eb7618f88b86")
    ]
 )
