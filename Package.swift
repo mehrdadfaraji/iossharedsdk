18c686a0f7a434873ad7170df6c4d85283a10a5b
Saeed yaftian <saeed@curatio.me>
2024-10-11 12:11:51 -0700

    Add new regeion to the link 8.0.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "datasourceProgramContactInformation",
-         url: "https://multi-platform-ios.s3.us-east-2.amazonaws.com/datasourceProgramContactInformation.xcframework.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARH3B77MKJZOM6UNJ%2F20241011%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20241011T000523Z&X-Amz-Expires=2592000&X-Amz-SignedHeaders=host&X-Amz-Signature=545bba176bd314ba77c43967f493ee670aabafa60b48227d0c0c55d8bf888ec9",
+         url: "https://multi-platform-ios.s3.us-east-1.amazonaws.com/datasourceProgramContactInformation.xcframework.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARH3B77MKJZOM6UNJ%2F20241011%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241011T190604Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=2bcc594a8358e797b7f1ce74b31a394a264adecde1f13e7ae5ddd12d91155a0b",
          checksum:"6bbfb1dbd03424a486d36714a32430992ef9b62da8019053ceb4eb7618f88b86")
    ]
 )
