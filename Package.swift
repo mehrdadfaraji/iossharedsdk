dc00d68c85bfb98b7438fde9c18b76364cca7cb4
Saeed yaftian <saeed@curatio.me>
2024-10-10 17:33:28 -0700

    New link is added with the new amazon s3

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "datasourceProgramContactInformation",
-         url: "https://kmm-shared-test.s3.us-east-2.amazonaws.com/datasourceProgramContactInformation.xcframework.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASFUIR2Q5Q5HE3RLT%2F20240826%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20240826T165745Z&X-Amz-Expires=174000&X-Amz-SignedHeaders=host&X-Amz-Signature=760c4fb654448416ee70014846bb7d6350391f9118a3d4a658f3e52ab72cb597",
+         url: "https://multi-platform-ios.s3.us-east-2.amazonaws.com/datasourceProgramContactInformation.xcframework.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARH3B77MKJZOM6UNJ%2F20241011%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20241011T000523Z&X-Amz-Expires=2592000&X-Amz-SignedHeaders=host&X-Amz-Signature=545bba176bd314ba77c43967f493ee670aabafa60b48227d0c0c55d8bf888ec9",
          checksum:"6bbfb1dbd03424a486d36714a32430992ef9b62da8019053ceb4eb7618f88b86")
    ]
 )
