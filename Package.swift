308ae14ce32555acbdb946be5c85d61f31f863e7
Saeed yaftian <saeed@curatio.me>
2024-10-23 14:28:08 -0700

    Add new framework with 9.2.0 tag

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "datasourceProgramContactInformation",
-         url: "https://multi-platform-ios.s3.us-east-1.amazonaws.com/datasourceProgramContactInformation.xcframework.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARH3B77MKJZOM6UNJ%2F20241021%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241021T215720Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=28e1c36020b12da4599156844730f4b2455d9de945ce45319a99377e59dc5711",
-         checksum:"83b0bd8e2a9ea99f3fa17d8b98b95b3a82f13d4d193e2df76d39bde49e9e18db")
+         url: "https://multi-platform-ios.s3.us-east-1.amazonaws.com/datasourceProgramContactInformation.xcframework.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARH3B77MKJZOM6UNJ%2F20241023%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241023T212451Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=ab3503ea3c487e597dcf957cf76782cf112ac71dc64388e82fec1ed56eab0fdf",
+         checksum:"24d4d7dcc2e152382d3b692df5a048c4265509e9a99d62e1a1f8f4fac09e9ea2")
    ]
 )
